157083838f126f09bacdffd7ad6c7c1d949726f7
Kye Hohenberger <kye@sideway.com>
2017-06-14 15:40:37 -0600

    emotion

--- packages/benchmarks/glam/simple/client/Table.js
+++ packages/benchmarks/glam/simple/client/Table.js
@@ -1,35 +1,32 @@
 import React from 'react';
-import css from 'glam';
+import { styled } from 'emotion';
 
-const tableClass = css`
+const Table = styled.div`
   display: table;
   margin-top: 10px;
 `;
 
-const rowClass = css`
+const Row = styled.div`
   display: table-row;
 `;
 
-const cellClass = css`
+const CellDefault = styled.div`
   display: table-cell;
   padding: 10px;
 `;
 
-const Table = ({ table, toPercent }) => (
-  <div className={tableClass}>
-    {table.map((row, i) => (
-      <div className={rowClass} key={i}>
-        {row.map((x, j) => {
-          const cellBgClass = css`background: rgba(74, 174, 53, ${x})`;
-          return (
-            <div className={`${cellClass} ${cellBgClass}`} key={`${i}${j}`}>
-              {toPercent(x)}
-            </div>
-          );
-        })}
-      </div>
-    ))}
-  </div>
-);
+const Cell = styled(CellDefault)`
+   background: rgba(74, 174, 53, ${props => props.value});
+`;
 
-export default Table;
+export default ({ table, toPercent }) => (
+  <Table>
+    {table.map((row, i) => (
+      <Row key={i}>
+        {row.map((x, j) => (
+          <Cell key={`${i}${j}`} value={x}>{toPercent(x)}</Cell>
+        ))}
+      </Row>
+    ))}
+  </Table>
+);
